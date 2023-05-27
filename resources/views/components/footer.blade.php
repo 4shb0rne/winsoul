1999352424a3cc844c8f2febb1b9a328cd3c146c
ashborne <susannoizanagi7@gmail.com>
2023-05-27 22:25:26 +0700

    change footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,17 @@
 <div class="footer">
-    <div class="footerchild">
+    <div class="footerchild text-center">
         <a class="navbar-brand" href="" style="height: 50px">
         </a>
-        &copy; Winsoul Ltd. 2023
+        <div>
+            Follow Us
+        </div>
+        <div class="mt-2" style="display: inline-block;">
+            <a class="text-dark" href="https://www.facebook.com/Linsoulaudio/"><i class="fab fa-facebook" style="font-size: 2em; margin-right: 15px"></i></a>
+            <a class="text-dark" href="https://www.instagram.com/linsoul.audio/?hl=en"><i class="fab fa-instagram" style="font-size: 2em; margin-right: 15px"></i></a>
+            <a class="text-dark" href="https://twitter.com/Linsoul_Audio"><i class="fab fa-twitter" style="font-size: 2em; margin-right: 15px"></i></a>
+        </div>
+        <div class="mt-2">
+            &copy; Winsoul Ltd. 2023
+        </div>
     </div>
 </div>
